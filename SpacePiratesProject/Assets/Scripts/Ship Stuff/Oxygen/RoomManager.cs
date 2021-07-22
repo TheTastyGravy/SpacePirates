ca13d81d84dd385b0c104e57e976bd536d1206e8
TheTastyGravy <thomasdufresne23@gmail.com>
2021-07-22 12:52:21 +1000

    Add oxygen system with hull holes.
     The oxygen system is used to track the ammount of oxygen in each room with
     hull holes reducing the amount of oxygen in the room until it gets too
     lower and players start getting damaged.
    
     The system is missing the ability to damage players as players dont have
     health yet.

---
+++ SpacePiratesProject/Assets/Scripts/Ship Stuff/Oxygen/RoomManager.cs
@@ -0,0 +1,109 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomManager : MonoBehaviour
+{
+    // All the positions a hole can be created at
+    public Transform[] holePositions;
+    // The possible hole prefabs to create
+    public GameObject[] holePrefabs;
+    [Space]
+    public float baseOxygenRegenRate = 10;
+    [Tooltip("When oxygen is below this level, players will start taking damage")]
+    public float oxygenDamageLevel = 10;
+
+
+    private HullHoleStation[] holes;
+    private float oxygenLevel = 100;
+
+    private List<BasicController> players = new List<BasicController>();
+
+    
+
+    public void DamageRoom()
+	{
+        int posIndex = Random.Range(0, holePositions.Length);
+
+        // If the hole already exists make it bigger, otherwise make a new hole
+        if (holes[posIndex] != null)
+		{
+            holes[posIndex].IncreaseHoleSize();
+        }
+		else
+		{
+            CreateHole(posIndex);
+        }
+    }
+
+    private void CreateHole(int index)
+	{
+        GameObject prefab = holePrefabs[Random.Range(0, holePrefabs.Length)];
+
+        // Create hole at position and get station script
+        HullHoleStation hole = Instantiate(prefab, holePositions[index].position, holePositions[index].rotation, transform).GetComponent<HullHoleStation>();
+        // Set up hole
+        holes[index] = hole;
+        hole.destroied = () => OnHoleDestroied(index);
+    }
+    // Called when a hole has been reparied
+    private void OnHoleDestroied(int index)
+	{
+        holes[index] = null;
+	}
+
+
+    void Start()
+    {
+        // There can be the same number of holes as hole positions
+        holes = new HullHoleStation[holePositions.Length];
+    }
+
+    void Update()
+    {
+        // Increase level by base amount
+        oxygenLevel += baseOxygenRegenRate * Time.deltaTime;
+        if (oxygenLevel > 100)
+            oxygenLevel = 100;
+
+        // Find the total loss rate
+        float oxygenLossRate = 0;
+        foreach (var hole in holes)
+		{
+            if (hole != null)
+			{
+                oxygenLossRate += hole.oxygenLossRate;
+            }
+		}
+        // Decrease level by loss rate
+        oxygenLevel -= oxygenLossRate * Time.deltaTime;
+        if (oxygenLevel < 0)
+            oxygenLevel = 0;
+
+
+        // If level is low enough, damage players in this room
+        if (oxygenLevel < oxygenDamageLevel)
+		{
+            foreach (var player in players)
+			{
+                //player.damage()
+			}
+        }
+    }
+
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("Player"))
+		{
+            //add player
+		}
+	}
+	void OnTriggerExit(Collider other)
+	{
+        if (other.CompareTag("Player"))
+        {
+            //remove player
+        }
+    }
+}
---
+++ SpacePiratesProject/Assets/Scripts/Ship Stuff/Stations/HullHoleStation.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HullHoleStation : Interactable
+{
+    public delegate void BasicDelegate();
+    public BasicDelegate destroied;
+
+    [System.Serializable]
+    public struct DamageLevel
+	{
+        public float oxygenLossRate;
+        public int repairCount;
+	}
+    public DamageLevel[] damageLevels;
+
+    [HideInInspector]
+    public float oxygenLossRate;
+    private int repairCount;
+
+    private int size = 0;
+    private int currentRepairCount = 0;
+
+
+
+    void Start()
+    {
+        oxygenLossRate = damageLevels[0].oxygenLossRate;
+        repairCount = damageLevels[0].repairCount;
+    }
+
+    public override void Activate(Interactor user)
+	{
+        currentRepairCount++;
+
+        // When fully repaired, reduce the hole size
+        if (currentRepairCount >= repairCount)
+		{
+            DecreaseHoleSize();
+		}
+	}
+
+    public void IncreaseHoleSize()
+	{
+        size++;
+        if (size >= damageLevels.Length)
+            size = damageLevels.Length - 1;
+
+        oxygenLossRate = damageLevels[size].oxygenLossRate;
+        repairCount = damageLevels[0].repairCount;
+        currentRepairCount = 0;
+    }
+    private void DecreaseHoleSize()
+	{
+        size--;
+        if (size < 0)
+		{
+            destroied();
+            Destroy(gameObject);
+        }
+		else
+		{
+            oxygenLossRate = damageLevels[size].oxygenLossRate;
+            repairCount = damageLevels[0].repairCount;
+            currentRepairCount = 0;
+        }
+	}
+}
