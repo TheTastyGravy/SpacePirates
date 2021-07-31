09622d0893e3bbcf3df47d2c3c1a9e0dd09ebcdd
TheTastyGravy <thomasdufresne23@gmail.com>
2021-07-31 17:28:32 +1000

    [CODE][ADD][Add UI for engine power, Fix characters]

--- SpacePiratesProject/Assets/Scripts/Ship Stuff/Stations/EngineStation.cs
+++ SpacePiratesProject/Assets/Scripts/Ship Stuff/Stations/EngineStation.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class EngineStation : Interactable
 {
@@ -9,19 +10,51 @@ public class EngineStation : Interactable
     public int PowerLevel { get => powerLevel; }
 
     public DamageStation damage;
+	public TextMeshProUGUI powerLabel;
 
 
+
+	void Start()
+	{
+		UpdateText();
+	}
 
 	protected override void OnActivate(Interactor user)
     {
         // Increase power level, looping
         powerLevel++;
 
-        bool isDamaged = damage != null;
+        bool isDamaged = damage == null;
         if (!isDamaged)
             isDamaged = damage.DamageLevel > 0;
         // If we are damaged, there are 2 power levels
         if (powerLevel > (isDamaged ? 1 : 2))
             powerLevel = 0;
-    }
+
+		UpdateText();
+	}
+
+	private void UpdateText()
+	{
+		if (powerLabel == null)
+			return;
+
+		Color color = Color.white;
+		switch (powerLevel)
+		{
+			case 0:
+				color = new Color(0, 0.5f, 0); //dark green
+				break;
+			case 1:
+				color = new Color(0.8f, 0.8f, 0); //dark yellow
+				break;
+			case 2:
+				color = Color.red;
+				break;
+		}
+
+		// Set text and color
+		powerLabel.text = (powerLevel + 1).ToString();
+		powerLabel.color = color;
+	}
 }
