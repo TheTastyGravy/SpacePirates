d3af85902d4c7d3c9de3c68fa4ed1acd35afb464
Len Farag <glizzyzbro@hotmail.com>
2021-08-02 11:00:13 +1000

    [CODE][ADD][Start work on Maneuver Queue]

--- SpacePiratesProject/Assets/Scripts/SceneControllers/HUDManeuverDisplay.cs
+++ SpacePiratesProject/Assets/Scripts/SceneControllers/HUDManeuverDisplay.cs
@@ -8,4 +8,26 @@ public class HUDManeuverDisplay : MonoBehaviour
     public TextMeshPro TimeUntilManeuver;
     public GameObject[] TrackManeuvers;
 
+    private void Awake()
+    {
+        m_Animation = GetComponent< Animation >();
+        TriggerSlideAnimation();
+    }
+
+    public void TriggerSlideAnimation()
+    {
+        m_Animation.Play( "SlideManeuverCards" );
+    }
+
+    private void AfterFade()
+    {
+
+    }
+
+    private void AfterSlide()
+    {
+
+    }
+
+    private Animation m_Animation;
 }
