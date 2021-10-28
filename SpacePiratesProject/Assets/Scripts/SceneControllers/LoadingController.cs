fcc550ced4d0fc9aedcf982d4084ad9be2609c34
Thomas Dufresne <thomasdufresne23@gmail.com>
2021-10-29 09:09:57 +1100

    [SCENE][ADD][Add continue prompt to loading scene]

--- SpacePiratesProject/Assets/Scripts/SceneControllers/LoadingController.cs
+++ SpacePiratesProject/Assets/Scripts/SceneControllers/LoadingController.cs
@@ -2,14 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class LoadingController : MonoBehaviour
 {
+    public TextMeshProUGUI continueText;
+
 
 
     void Start()
     {
         Player.GetPlayerBySlot(Player.PlayerSlot.P1).AddInputListener(Player.Control.A_PRESSED, OnAPressed);
+        InvokeRepeating(nameof(ToggleText), 0.5f, 0.5f);
     }
 
     void OnDestroy()
@@ -20,5 +24,10 @@ public class LoadingController : MonoBehaviour
     private void OnAPressed(InputAction.CallbackContext context)
     {
         GameManager.ChangeState(GameManager.GameState.GAME);
+    }
+
+    private void ToggleText()
+    {
+        continueText.enabled = !continueText.enabled;
     }
 }
