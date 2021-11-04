2f6d9d539778dbdcd83c59d336121ffebafbdb7c
Suqnier <sl00ka@hotmail.com>
2021-11-04 16:15:42 +1100

    [SCENE/CODE][UPDATE/ADD][correction of right turret fire arc, adition of recoil script to fire point prefab and script]

---
+++ SpacePiratesProject/Assets/Scripts/MiscEnvironment/TurretRecoil.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class TurretRecoil : MonoBehaviour
+{
+    public GameObject Barrel;
+    public Vector3 RecoilAmount;
+    public float Speed;
+
+    Vector3 startPos, endPos, targetPos;
+    bool running;
+
+    private void OnEnable()
+    {
+        startPos = Barrel.transform.localPosition;
+        endPos = startPos + RecoilAmount;
+        running = false;
+    }
+
+    private void FixedUpdate()
+    {
+        if (running)
+        {
+            if (transform.position != targetPos)
+                transform.position = Vector3.MoveTowards(transform.position, targetPos, Speed * Time.fixedDeltaTime);
+            else
+                Stop();
+        }
+    }
+
+    public void Run()
+    {
+        running = true;
+        Barrel.transform.position = startPos;
+        targetPos = endPos;
+    }
+
+    void Stop()
+    {
+        if (transform.position == startPos)
+            running = false;
+        else
+            targetPos = startPos;
+    }
+}
