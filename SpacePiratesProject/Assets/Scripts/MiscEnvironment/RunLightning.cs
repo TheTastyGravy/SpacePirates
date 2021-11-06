0593269521b8e7a4fbc40fdaa2722d8e45a46ce8
Suqnier <s200482@students.aie.edu.au>
2021-11-06 22:58:40 +1100

    [SCENE][UPDATE][Plasma storms now have a varied selection of light animations to play]

--- SpacePiratesProject/Assets/Scripts/MiscEnvironment/RunLightning.cs
+++ SpacePiratesProject/Assets/Scripts/MiscEnvironment/RunLightning.cs
@@ -4,22 +4,23 @@ public class RunLightning : MonoBehaviour
 {
     public PlasmaLightLogic[] _Lights;
     [Space]
-    public float[] LightTriggers;
+    [SerializeField] LightArrays[] LightTriggers;
     public float LightDelay;
     [Space]
     public float RandomBufferMin;
     public float RandomBufferMax;
 
     float elapsed, targetTime;
-    int iterative;
+    int iterative, selective;
     private void OnEnable()
     {
         elapsed = 0f;
         iterative = 0;
+        selective = Random.Range(0, LightTriggers.Length);
         targetTime = GetStartTime();
         InitializeLights();
     }
-    float GetStartTime() => LightTriggers[0] + Random.Range(RandomBufferMin, RandomBufferMax);
+    float GetStartTime() => LightTriggers[selective].LightTriggers[0] + Random.Range(RandomBufferMin, RandomBufferMax);
     void InitializeLights()
     {
         for (int i = 0; i < _Lights.Length; i++)
@@ -39,12 +40,21 @@ public class RunLightning : MonoBehaviour
     {
         elapsed = 0f;
         iterative++;
-        if (iterative >= LightTriggers.Length)
+        if (iterative >= LightTriggers[selective].LightTriggers.Length)
             iterative = 0;
-        if(iterative == 0)
+        if (iterative == 0)
+        {
+            selective = Random.Range(0, LightTriggers.Length);
             targetTime = GetStartTime();
+        }
         else
-            targetTime = LightTriggers[iterative];
+            targetTime = LightTriggers[selective].LightTriggers[iterative];
     }
     void RunLight() => _Lights[Random.Range(0, _Lights.Length)].Run();
+}
+[System.Serializable]
+struct LightArrays
+{
+    public string Heading;
+    public float[] LightTriggers;
 }
