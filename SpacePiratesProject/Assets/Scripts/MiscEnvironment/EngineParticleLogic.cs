b3d26758bef6faf73c3ad058a03e7e8188a20478
Suqnier <s200482@students.aie.edu.au>
2021-11-07 13:00:58 +1100

    [SCENE/CODE][UPDATE/ADD][update to engine flame prefab, implemented on all ships, script to run engine flames]

---
+++ SpacePiratesProject/Assets/Scripts/MiscEnvironment/EngineParticleLogic.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class EngineParticleLogic : MonoBehaviour
+{
+    public ParticleSystem Ignite, Flame;
+    [Space]
+    public float Delay;
+
+    float elapsed;
+
+    private void OnEnable()
+    {
+        elapsed = 0f;
+    }
+
+    private void FixedUpdate()
+    {
+        if (elapsed < Delay)
+        {
+            elapsed += Time.fixedDeltaTime;
+            if (elapsed > Delay)
+                Flame.Play();
+        }
+    }
+
+    public void Run()
+    {
+        Ignite.Play();
+        elapsed = 0f;
+    }
+
+    public void Stop()
+    {
+        Ignite.Stop();
+        Flame.Stop();
+    }
+}
