10f6230f398462e5bef4beb183001c6571715131
Suqnier <s200482@students.aie.edu.au>
2021-11-07 11:47:15 +1100

    [SCENE/CODE][ADD][Flash effect prefab and script to run]

---
+++ SpacePiratesProject/Assets/Scripts/MiscEnvironment/FlashLogic.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+public class FlashLogic : MonoBehaviour
+{
+    public Image _Image;
+    [Space]
+    public float Speed;
+
+    bool running, back;
+    private void OnEnable()
+    {
+        running = false;
+        back = false;
+    }
+
+    private void FixedUpdate()
+    {
+        if(running)
+        {
+            if (!back)
+            {
+                _Image.color += new Color(0f, 0f, 0f, Time.fixedDeltaTime * Speed);
+                if (_Image.color.a >= 1f)
+                    back = true;
+            }
+            else
+            {
+                _Image.color += new Color(0f, 0f, 0f, Time.fixedDeltaTime * -Speed);
+                if (_Image.color.a <= 0f)
+                    running = false;
+            }
+        }
+    }
+
+    public void Run()
+    {
+        running = true;
+        back = false;
+    }
+}
