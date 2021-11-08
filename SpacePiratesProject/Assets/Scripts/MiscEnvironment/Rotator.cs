073cac92edd09cba76ef371cbfe0792d69e86ae2
Suqnier <s200482@students.aie.edu.au>
2021-11-09 09:15:17 +1100

    [SCENE][UPDATE][applied asteroid textures, update hull breach, updated missile prefab with rotator and particle effect]

---
+++ SpacePiratesProject/Assets/Scripts/MiscEnvironment/Rotator.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    public GameObject _Object;
+    public Vector3 _Dir;
+    public float _Speed;
+
+    Transform myTransform;
+    private void OnEnable() => myTransform = _Object.transform;
+
+    private void FixedUpdate() => myTransform.Rotate(_Dir * _Speed * Time.fixedDeltaTime);
+}
