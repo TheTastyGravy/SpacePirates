523648ce1eeae74e4f3786b62ac2c05480d591b2
Suqnier <sl00ka@hotmail.com>
2021-11-12 15:14:31 +1100

    [SCENE/CODE][ADD][update to settings vibration name and on/off indicator and code]

---
+++ SpacePiratesProject/Assets/Scripts/MiscEnvironment/MenuVibrationbsLogic.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class MenuVibrationbsLogic : MonoBehaviour
+{
+    public TextMeshProUGUI Display;
+    public Toggle Ref;
+    [Space]
+    public string on, off;
+    private void OnEnable() => Change();
+
+    public void Change() => Display.text = Ref.isOn ? on : off;
+}
