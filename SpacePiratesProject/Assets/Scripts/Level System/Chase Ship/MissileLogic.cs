427dc4013b40e229fb442999bdbc31bb22cab0b3
Thomas <cnpowerman23@gmail.com>
2021-11-19 17:08:48 +1100

    [CODE][ADD][Make chase ship missiles homing]

--- SpacePiratesProject/Assets/Scripts/Level System/Chase Ship/MissileLogic.cs
+++ SpacePiratesProject/Assets/Scripts/Level System/Chase Ship/MissileLogic.cs
@@ -8,11 +8,14 @@ public class MissileLogic : MonoBehaviour
 	[Tooltip("How many times it can be hit by a turret before being destroied")]
 	public int health = 1;
 	public float speed = 10;
+	public float steeringSpeed = 30;
+	public float angleFactorMultiplier = 1;
 	[Space]
 	public GameObject explosion;
 	public Transform magicCollider;
 
-
+	private Transform trans;
+	private Transform shipTrans;
 	private bool hasHitShip = false;
 
 
@@ -22,12 +25,20 @@ public class MissileLogic : MonoBehaviour
 		// Allign collider with camera
 		magicCollider.rotation = Camera.main.transform.rotation;
 		Destroy(gameObject, 10);
+		trans = transform;
+		shipTrans = ShipManager.Instance.transform;
 	}
 
 	void Update()
 	{
+		// Rotate the forward direction to try andfact the player ship
+		Vector3 dir = shipTrans.position - trans.position;
+		float angleFactor = Mathf.Abs(Vector3.Angle(trans.forward, dir) * Mathf.Deg2Rad) * angleFactorMultiplier;
+		dir = Vector3.RotateTowards(trans.forward, dir, steeringSpeed * Mathf.Deg2Rad * Time.deltaTime * angleFactor, 0);
+		dir.y = 0;
+		trans.forward = dir;
 		// Update position
-		transform.position += speed * Time.deltaTime * transform.forward;
+		trans.position += speed * Time.deltaTime * trans.forward;
 	}
 
 	void OnCollisionEnter(Collision collision)
