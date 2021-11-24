b40221fa56512b5f552320923721bd5b0197728a
Thomas <cnpowerman23@gmail.com>
2021-11-24 13:59:12 +1100

    [CODE][ADD][Add object pool debuging]

--- SpacePiratesProject/Assets/Scripts/Common/ObjectPool.cs
+++ SpacePiratesProject/Assets/Scripts/Common/ObjectPool.cs
@@ -10,7 +10,7 @@ public class ObjectPool : MonoBehaviour
     [Tooltip("The maximum number of objects that will be stored as disabled")]
     public uint maxStoredCount = 10;
 
-    private List<GameObject> pool = new List<GameObject>();
+    public List<GameObject> pool = new List<GameObject>();
     // Object to be returned to the pool after a delay
     private struct ReturnEntry
     {
@@ -63,6 +63,12 @@ public class ObjectPool : MonoBehaviour
         if (pool.Count > 0)
         {
             GameObject obj = pool[0];
+
+            if (obj == null)
+            {
+                Debug.LogError("<size=15>OBJECT POOL INSTANCE IS NULL</size>");
+            }
+
             obj.SetActive(true);
             pool.RemoveAt(0);
             return obj;
@@ -76,6 +82,11 @@ public class ObjectPool : MonoBehaviour
 
     public void Return(GameObject instance)
     {
+        if (instance == null)
+        {
+            Debug.LogError("<size=15>OBJECT POOL INSTANCE IS NULL</size>");
+        }
+
         if (pool.Count < maxStoredCount)
         {
             instance.SetActive(false);
@@ -90,6 +101,11 @@ public class ObjectPool : MonoBehaviour
 
     public void Return(GameObject instance, float time)
     {
+        if (instance == null)
+        {
+            Debug.LogError("<size=15>OBJECT POOL INSTANCE IS NULL</size>");
+        }
+
         toReturn.Add(new ReturnEntry() { obj = instance, timeRemaining = time });
     }
 
